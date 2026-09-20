1ce2072ef07274bced60643357b2e005f11b599c
ifcquery <fbnge4@gmail.com>
2026-09-20 17:30:13 +0200

    update villa example: use xeoIFC to create and modify IFC models

--- docs/assets/node.browser-D3-r3ftm.js
+++
@@ -1,1 +0,0 @@
-import{A as o}from"./index-Blnjf1Ay.js";import"./index-yT8AFkiK.js";function n(e){return new Proxy({},{get(i,t){if(typeof t!="symbol")throw new o(`\`${e}.${t}\` is not available in this environment; it needs a Node.js-compatible runtime`)}})}const s=n("fs"),f=n("path");export{s as fs,f as path};
