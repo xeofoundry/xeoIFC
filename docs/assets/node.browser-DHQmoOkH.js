444bea5ce4da7e3d3dcb483f6857e585e0e7fdcb
ifcquery <fbnge4@gmail.com>
2026-09-21 11:51:46 +0200

    update WebAssembly IFC viewer

--- docs/assets/node.browser-DHQmoOkH.js
+++
@@ -1,1 +0,0 @@
-import{A as o}from"./index-DbBjrrYO.js";import"./index-CSPYaI6m.js";function n(e){return new Proxy({},{get(i,t){if(typeof t!="symbol")throw new o(`\`${e}.${t}\` is not available in this environment; it needs a Node.js-compatible runtime`)}})}const s=n("fs"),f=n("path");export{s as fs,f as path};
